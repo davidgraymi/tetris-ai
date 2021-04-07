eab470887e0a00815951d537d88f6c7c2d0f7e40
David Gray <54521366+davidgraymi@users.noreply.github.com>
2021-04-07 17:41:10 -0500

    organization

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ read -p "Enter commit message: " message
 printf "\e[33m\nPushing to tetris-ai repository...\e[39m\n\n"
 git add .
 git commit -m "$message"
-git push origin master
+git push
 
 printf "\e[32m\nSuccessfully deployed!\e[39m"
