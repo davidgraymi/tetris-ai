e271551f2134fa053d94fd4ff80a2ab03201575d
David Gray <54521366+davidgraymi@users.noreply.github.com>
2021-04-20 14:14:19 -0500

    teris_env notes

--- src/tetris_env.py
+++ src/tetris_env.py
@@ -1,11 +1,35 @@
 from gym import Env
 from gym.spaces import Discrete, Box
+import numpy as np
 from tetris import MyGame
 import arcade
 
 class TetrisEnv(Env):
 
+    """
+    Observation:
+        None
+
+    Actions:
+        Type: Discrete(5)
+        Num   Action
+        0     Push shape left
+        1     Push shape right
+        2     Push shape down
+        3     Rotate shape
+
+    Reward:
+        Reward is 1 for every row is completed
+
+    Starting State:
+        Empty board
+
+    Episode Termination:
+        A shape fills the top square in the play area.
+    """
+
     def __init__(self):
+
         # Actions we can take: left, right, up, down
         self.action_space = Discrete(4)
 
