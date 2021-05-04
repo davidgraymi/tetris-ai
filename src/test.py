07f6611982bffee8eec262ef135fd0e6263a7eec
David Gray <54521366+davidgraymi@users.noreply.github.com>
2021-05-04 11:21:12 -0500

    test.py

--- src/test.py
+++ src/test.py
@@ -2,8 +2,9 @@ from tetris import Tetris
 from dqn_agent import DQNAgent
 
 env = Tetris()
-episodes = 1
+episodes = 10
 max_steps = None
+epsilon = 0
 epsilon_stop_episode = 1500
 mem_size = 20000
 discount = 0.95
@@ -13,17 +14,19 @@ epochs = 1
 replay_start_size = 2000
 # train_every = 1
 n_neurons = [32, 32]
-render_delay = 0.1
+render_delay = None
 activations = ['relu', 'relu', 'linear']
-# dqn_num = 1
-# filepaths = ["tetris-nn_"+str(dqn_num)+"-"+str(i)+".h5" for i in range(0,10)]
+dqn_num = 1
+filepaths = ["tetris-nn_"+str(dqn_num)+"-"+str(i) for i in range(0,10)]
 # save = len(filepaths)
 # save_every = episodes/save
 
 agent = DQNAgent(env.get_action_space(),
-                     n_neurons=n_neurons, activations=activations,
+                     n_neurons=n_neurons, activations=activations, epsilon=0,
                      epsilon_stop_episode=epsilon_stop_episode, mem_size=mem_size,
                      discount=discount, replay_start_size=replay_start_size)
+
+agent.load(filepaths[-2])
 
 scores = []
 
@@ -35,7 +38,9 @@ for episode in range(episodes):
     # Game
     while not done and (not max_steps or steps < max_steps):
         next_states = env.get_next_states()
+        print(next_states)
         best_state = agent.best_state(next_states.values())
+        print(best_state)
         
         best_action = None
         for action, state in next_states.items():
